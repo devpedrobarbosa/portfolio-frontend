e79ad6a08a92f3490a06c2cd0aeb31353385568b
devpedrobarbosa <1230110831@veigadealmeida.edu.br>
2025-02-28 01:55:02 -0300

    add home nav

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@
         <nav>
             <div class="logo">&lt;/&gt;</div>
             <ul class="nav-links">
+                <li><a href="#">Home</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
