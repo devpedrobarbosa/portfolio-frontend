c24e45d5330af35bf434de9cb417b228698b9d4a
devpedrobarbosa <1230110831@veigadealmeida.edu.br>
2025-03-24 22:08:16 -0300

    testin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+
+// Load environment variables
+const env = dotenv.config()
+dotenvExpand.expand(env)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +20,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  define: {
+    'import.meta.env.VITE_ADMIN_PWD': JSON.stringify(process.env.VITE_ADMIN_PWD || '')
   },
 })
