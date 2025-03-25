d938d57e901df0470bb5d5050dd164aadf8037f4
devpedrobarbosa <1230110831@veigadealmeida.edu.br>
2025-03-24 22:12:05 -0300

    testin some more

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import dotenv from 'dotenv'
-import dotenvExpand from 'dotenv-expand'
-
-// Load environment variables
-const env = dotenv.config()
-dotenvExpand.expand(env)
 
 // https://vite.dev/config/
 export default defineConfig({
